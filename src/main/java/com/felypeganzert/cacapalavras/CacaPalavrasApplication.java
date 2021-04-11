68b9b47d49f31d604ffdac6078c02ac6508931ed
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2021-04-11 03:45:54 -0300

    Created Spring Boot project

---
+++ src/main/java/com/felypeganzert/cacapalavras/CacaPalavrasApplication.java
@@ -0,0 +1,13 @@
+package com.felypeganzert.cacapalavras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacaPalavrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacaPalavrasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felypeganzert/cacapalavras/CacaPalavrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felypeganzert.cacapalavras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacaPalavrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
