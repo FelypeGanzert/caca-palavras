36fcf1ec9cd3348490067c9d5b2921906aba2a6d
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2021-04-17 20:09:23 -0300

    cria enum Direcoes com as 9 direcoes basicas

---
+++ src/main/java/com/felypeganzert/cacapalavras/entidades/Direcao.java
@@ -0,0 +1,24 @@
+package com.felypeganzert.cacapalavras.entidades;
+
+import lombok.Getter;
+
+@Getter
+public enum Direcao {
+
+    // NOROESTE NORTE NORDESTE
+    // OESTE CENTRO LESTE
+    // SUDOESTE SUL SUDESTE
+
+    NOROESTE(-1,-1), NORTE(0,-1), NORDESTE(1,-1),
+    OESTE(-1,0), CENTRO(0,0), LESTE(1,0),
+    SUDOESTE(-1,1), SUL(0,1), SUDESTE(1,1);
+
+    private int x;
+    private int y;
+
+    private Direcao(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+}
