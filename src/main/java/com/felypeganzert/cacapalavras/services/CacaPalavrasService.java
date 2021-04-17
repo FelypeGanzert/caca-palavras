21760d35495eef29942eabd47a6867616369744c
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2021-04-17 12:25:05 -0300

    remove metodo de CacaPalavraService

--- src/main/java/com/felypeganzert/cacapalavras/services/CacaPalavrasService.java
+++ src/main/java/com/felypeganzert/cacapalavras/services/CacaPalavrasService.java
@@ -2,10 +2,12 @@ package com.felypeganzert.cacapalavras.services;
 
 import com.felypeganzert.cacapalavras.entidades.CacaPalavras;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface CacaPalavrasService {
 
     void encontrarPalavrasNoTabuleiro(CacaPalavras cacaPalavras);
     void limparLetrasDoTabuleiro(CacaPalavras cacaPalavras);
-    void alterarDimensoesDoTabuleiro(CacaPalavras cacaPalavras);
     
 }
