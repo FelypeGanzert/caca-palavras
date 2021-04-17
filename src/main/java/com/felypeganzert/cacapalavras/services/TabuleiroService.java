3c095620d95950e9a418a6e53383002949d0683a
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2021-04-17 12:25:29 -0300

    adiciona anotacoes em Tabuleiro e TabuleiroService

--- src/main/java/com/felypeganzert/cacapalavras/services/TabuleiroService.java
+++ src/main/java/com/felypeganzert/cacapalavras/services/TabuleiroService.java
@@ -3,6 +3,9 @@ package com.felypeganzert.cacapalavras.services;
 import com.felypeganzert.cacapalavras.entidades.Letra;
 import com.felypeganzert.cacapalavras.entidades.Tabuleiro;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface TabuleiroService {
     
     void inserirLetraEmCelula(Tabuleiro tabuleiro,  Letra letra);
