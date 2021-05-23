ae25ddf23f44178217876802a9a818ff7780b46c
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2021-05-23 20:51:57 -0300

    Remove JpaRepository de entidade Embedable

--- src/main/java/com/felypeganzert/cacapalavras/repository/PosicaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.felypeganzert.cacapalavras.repository;
-
-import com.felypeganzert.cacapalavras.entidades.Posicao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PosicaoRepository extends JpaRepository<Posicao, Long>{
-    
-}
