e60ea9f0df0ea0f9ccffa4ccc01788a8ec9c1117
Felype Ganzert <felipe.ganzert@gmail.com>
2021-09-04 16:19:05 -0300

    Create AppConstantes.java

---
+++ src/main/java/com/felypeganzert/cacapalavras/services/impl/AppConstantes.java
@@ -0,0 +1,11 @@
+package com.felypeganzert.cacapalavras.services.impl;
+
+public class AppConstantes {
+    
+    public static final String ID = "id";
+
+    public static final String CACA_PALAVRAS = "Caça Palavras";
+
+    
+
+}
