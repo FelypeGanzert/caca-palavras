81498057732a4858153a3ad0304d4f306f0ce2e9
Felype Ganzert <felipe.ganzert@gmail.com>
2021-09-04 16:39:12 -0300

    Corrige pacote de AppContantes e remove linhas em branco

--- src/main/java/com/felypeganzert/cacapalavras/utils/AppConstantes.java
+++ src/main/java/com/felypeganzert/cacapalavras/utils/AppConstantes.java
@@ -1,11 +1,9 @@
-package com.felypeganzert.cacapalavras.services.impl;
+package com.felypeganzert.cacapalavras.utils;
 
 public class AppConstantes {
-    
+
     public static final String ID = "id";
 
     public static final String CACA_PALAVRAS = "Caça Palavras";
-
-    
 
 }
