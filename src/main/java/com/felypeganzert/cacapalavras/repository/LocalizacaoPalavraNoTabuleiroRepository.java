0963bf277044e451ea6f0bb0fde51a83e0e1e79b
Felype Ganzert <felipe.ganzert@gmail.com>
2021-09-08 21:47:32 -0300

    Atualiza LocalizacaoPalavraNoTabuleiroRepository com métodoos de delete

--- src/main/java/com/felypeganzert/cacapalavras/repository/LocalizacaoPalavraNoTabuleiroRepository.java
+++ src/main/java/com/felypeganzert/cacapalavras/repository/LocalizacaoPalavraNoTabuleiroRepository.java
@@ -3,7 +3,22 @@ package com.felypeganzert.cacapalavras.repository;
 import com.felypeganzert.cacapalavras.entidades.LocalizacaoPalavraNoTabuleiro;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LocalizacaoPalavraNoTabuleiroRepository extends JpaRepository<LocalizacaoPalavraNoTabuleiro, Integer>{
     
+    @Modifying
+    @Query("DELETE FROM LocalizacaoPalavraNoTabuleiro l WHERE l.palavra.cacaPalavras.tabuleiro.id = :idTabuleiro")
+    void deleteAllFromTabuleiroId(Integer idTabuleiro);
+
+    @Modifying
+    @Query(" DELETE FROM LocalizacaoPalavraNoTabuleiro locPalavra "
+           + " WHERE locPalavra.id IN ( "
+           + "      SELECT locPalavra2.id FROM LocalizacaoPalavraNoTabuleiro locPalavra2 "
+           + "      INNER JOIN locPalavra2.localizacoesLetrasNoTabuleiro locLetra2 "
+           + "      WHERE locLetra2.letra.id = :idLetra "
+           + " )")
+    void deleteAllUsingLetraId(Integer idLetra);
+
 }
