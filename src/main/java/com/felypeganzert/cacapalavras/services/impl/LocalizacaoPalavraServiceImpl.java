e27c59f78f28a2d442f01b145eee5f792f482303
Felype Ganzert <felipe.ganzert@gmail.com>
2021-09-11 18:35:01 -0300

    Implementa a lógica de alguns métodos de LocalizacaoPalavraServiceImpl

--- src/main/java/com/felypeganzert/cacapalavras/services/impl/LocalizacaoPalavraServiceImpl.java
+++ src/main/java/com/felypeganzert/cacapalavras/services/impl/LocalizacaoPalavraServiceImpl.java
@@ -2,6 +2,7 @@ package com.felypeganzert.cacapalavras.services.impl;
 
 import java.util.List;
 
+import com.felypeganzert.cacapalavras.repository.LocalizacaoPalavraRepository;
 import com.felypeganzert.cacapalavras.services.LocalizacaoPalavraService;
 
 import org.springframework.stereotype.Service;
@@ -11,25 +12,27 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class LocalizacaoPalavraServiceImpl implements LocalizacaoPalavraService {
+
+    private final LocalizacaoPalavraRepository repository;
     
     @Override
     public void deleteAllAssociadasAoTabuleiro(Integer idTabuleiro) {
-        // TODO Auto-generated method stub
+        repository.deleteAllFromTabuleiroId(idTabuleiro);
     }
 
     @Override
     public void deleteAllFromPalavra(Integer idPalavra) {
-        // TODO Auto-generated method stub
+        repository.deleteAllFromPalavraId(idPalavra);
     }
 
     @Override
     public void deleteAllUsandoLetra(Integer idLetra) {
-        // TODO Auto-generated method stub
+        repository.deleteAllUsingLetraId(idLetra);
     }
 
     @Override
     public void deleteAllUsandoLetras(List<Integer> idLetras) {
-        // TODO Auto-generated method stub
+        // TODO: criar um método no repository para isso
     }
 
 
