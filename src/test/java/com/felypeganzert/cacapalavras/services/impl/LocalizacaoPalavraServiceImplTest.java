4213106ad3e6de0ed7512aa682533ff7e0a499c6
Felype Ganzert <felipe.ganzert@gmail.com>
2021-09-26 00:05:25 -0300

    Implementa os testes em LocalizacaoPalavraServiceImplTest

--- src/test/java/com/felypeganzert/cacapalavras/services/impl/LocalizacaoPalavraServiceImplTest.java
+++ src/test/java/com/felypeganzert/cacapalavras/services/impl/LocalizacaoPalavraServiceImplTest.java
@@ -1,10 +1,17 @@
 package com.felypeganzert.cacapalavras.services.impl;
 
+import static org.mockito.Mockito.times;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import com.felypeganzert.cacapalavras.repository.LocalizacaoPalavraRepository;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -15,22 +22,39 @@ public class LocalizacaoPalavraServiceImplTest {
 
     @Mock
     private LocalizacaoPalavraRepository repository;
-
-    // TODO: implementar os testes listados abaixo:
     
-    // # deleteAllAssociadasAoTabuleiro
-    // deveChamarDeleteAllFromTabuleiroIdDoRepositoryComSucesso
+    @Test
+    void deveChamarDeleteAllFromTabuleiroIdDoRepositoryComSucesso(){
+        int idTabuleiro = 1;
+        service.deleteAllAssociadasAoTabuleiro(idTabuleiro);
+
+        Mockito.verify(repository, times(1)).deleteAllFromTabuleiroId(idTabuleiro);
+    }
      
-    // # deleteAllFromPalavra
-    // deveChamarDeleteAllFromPalavraIdDoRepositoryComSucesso
+    @Test
+    void deveChamarDeleteAllFromPalavraIdDoRepositoryComSucesso(){
+        int idPalavra = 1;
+        service.deleteAllFromPalavra(idPalavra);
 
-    // # findAll
-    // deveChamarFindByIdDoServiceTabuleiroERetornarTodasAsLetrasDoTabuleiro
+        Mockito.verify(repository, times(1)).deleteAllFromPalavraId(idPalavra);
+    }
     
-    // # deleteAllUsandoLetra
-    // deveChamarDeleteAllUsingLetraIdDoRepositoryComSucesso
+    @Test
+    void deveChamarDeleteAllUsingLetraIdDoRepositoryComSucesso(){
+        int idLetra = 1;
+        service.deleteAllUsandoLetra(idLetra);
+
+        Mockito.verify(repository, times(1)).deleteAllUsingLetraId(idLetra);
+    }
     
-    // # deleteAllUsandoLetras
-    // implementar lógica do método
+    @Test
+    void deveChamarDeleteAllUsingLetrasIdDoRepositoryComSucesso(){
+        List<Integer> idLetrasParaExcluir = new ArrayList<>();
+        idLetrasParaExcluir.add(1);
+        idLetrasParaExcluir.add(2);
+        service.deleteAllUsandoLetras(idLetrasParaExcluir);
+
+        Mockito.verify(repository, times(1)).deleteAllUsingLetrasId(idLetrasParaExcluir);
+    }
 
 }
