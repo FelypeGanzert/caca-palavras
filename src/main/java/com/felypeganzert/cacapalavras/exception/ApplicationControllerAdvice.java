2e06b72a3b80f6133353861a4b85ed0f8f952052
Felype Ganzert <felipe.ganzert@gmail.com>
2021-10-03 11:48:13 -0300

    Refatora ApplicationControllerAdvice e adiciona handleMethodArgumentNotValidException

--- src/main/java/com/felypeganzert/cacapalavras/exception/ApplicationControllerAdvice.java
+++ src/main/java/com/felypeganzert/cacapalavras/exception/ApplicationControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.felypeganzert.cacapalavras.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,30 +12,41 @@ public class ApplicationControllerAdvice {
 
     @ExceptionHandler(RegraNegocioException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ApiError resolveException(RegraNegocioException exc) {
-        ApiError apiError = new ApiError(exc.getMessage());
+	public ApiError handleRegraNegocioException(RegraNegocioException e) {
+        ApiError apiError = new ApiError(e.getMessage());
         return apiError;
 	}
     
     @ExceptionHandler(PalavraJaExisteNoCacaPalavrasException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ApiError resolveException(PalavraJaExisteNoCacaPalavrasException exc) {
-        ApiError apiError = new ApiError(exc.getMessage());
+	public ApiError handlePalavraJaExisteNoCacaPalavrasException(PalavraJaExisteNoCacaPalavrasException e) {
+        ApiError apiError = new ApiError(e.getMessage());
         return apiError;
 	}
 
     @ExceptionHandler(RecursoNaoEncontradoException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-	public ApiError resolveException(RecursoNaoEncontradoException exc) {
-        ApiError apiError = new ApiError(exc.getMessage());
+	public ApiError handleRecursoNaoEncontradoException(RecursoNaoEncontradoException e) {
+        ApiError apiError = new ApiError(e.getMessage());
         return apiError;
 	}
 
     @ExceptionHandler(RecursoNaoPertenceAException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ApiError resolveException(RecursoNaoPertenceAException exc) {
-        ApiError apiError = new ApiError(exc.getMessage());
+	public ApiError handleRecursoNaoPertenceAException(RecursoNaoPertenceAException e) {
+        ApiError apiError = new ApiError(e.getMessage());
         return apiError;
 	}
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ApiError apiError = new ApiError();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            apiError.getErros()
+                .add(fieldError.getDefaultMessage());
+        }
+        return apiError;
+    }
     
 }
